c583caf93c665930e1557092b72aa53388032b89
alaminas9291-star <alaminas9291@gmail.com>
2026-08-20 22:38:44 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Hero Button Click Action
+function showMessage() {
+    alert("কারিগরি বিদ্যাপাঠে আপনাকে স্বাগতম! অতি শীঘ্রই ২০২৬ শিক্ষাবর্ষের সকল ডিপ্লোমা কোর্সের অনলাইন রেজিস্ট্রেশন শুরু হবে।");
+}
+
+// Smooth Interactive Feedback
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("কারিগরি বিদ্যাপাঠ ওয়েবসাইট সফলভাবে লোড হয়েছে।");
+});
